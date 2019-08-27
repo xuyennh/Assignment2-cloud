f5cb4ecdd7aec7852578b8e62366e33f396fbcfb
xuyen1999bn <51747065+xuyen1999bn@users.noreply.github.com>
2019-08-27 20:43:14 +0700

    Update pg.js

--- pg.js
+++ pg.js
@@ -1,9 +1,9 @@
 const {Client, Pool} = require('pg')
 const client = new Client({
-    database: 'xuyen',
-    host: 'localhost',
-    user: 'postgres',
-    password: '2108',
+    database: 'd14uehlmob7ne4',
+    host: 'ec2-54-221-214-3.compute-1.amazonaws.com',
+    user: 'hxqatomyuhhglt',
+    password: '1a2774472077623f778d49f03b1da32b42d92ad0ffcb34dae7ef316952415697',
     port: 5433
 })
 
