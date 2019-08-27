927d76510badef169797b7a46d145a458ac80ba0
xuyen1999bn <51747065+xuyen1999bn@users.noreply.github.com>
2019-08-27 20:45:20 +0700

    Update pg.js

--- pg.js
+++ pg.js
@@ -4,7 +4,7 @@ const client = new Client({
     host: 'ec2-54-221-214-3.compute-1.amazonaws.com',
     user: 'hxqatomyuhhglt',
     password: '1a2774472077623f778d49f03b1da32b42d92ad0ffcb34dae7ef316952415697',
-    port: 5433
+    port: 5432
 })
 
 client.connect()
